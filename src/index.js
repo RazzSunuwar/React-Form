d521d936df1d1ca0938fdf6ec6808d09c3f2c28e
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-11 21:44:50 +0545

    Textarea

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Example: When you fill in your age as not numeric, an error message is displayed:
+// Example: A simple textarea with some content initialized in the constructor:
 
 class MyForm extends React.Component{
     constructor(props){
         super(props);
-        this.state = { 
-        username: '',
-        age: null,
-        errormessage: ''
+        this.state = {
+            description: 'The content of a textarea goes in the value attribute'
         };
-    }
+    };
 
-    myChangeHandler = (event) => {
-        let nam = event.target.name;
-        let val = event.target.value;
-        let err = '';
-        if (nam === "age") {
-            if (val !="" && !Number(val)) {
-                err = <strong>Your age must be a number</strong>;
-            }
-        }
-        this.setState({errormessage: err});
-        this.setState({[nam]: val});
-
-    }
     render() {
         return(
             <form>
-                <h1>Hello {this.state.username} {this.state.age}</h1>
-                <p>Enter your name:</p>
-                <input
-                type ="text"
-                name = "username"
-                onChange = {this.myChangeHandler}
-                />
-                <p>Enter the age:</p>
-                <input 
-                type = "text"
-                name = "age"
-                onChange = {this.myChangeHandler}
-                />
-                {this.state.errormessage}
+                <textarea value = {this.state.description} />
             </form>
         );
     };

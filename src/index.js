5ce3253193a9b42c8c9d0220e96bc6b3899dc509
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-11 11:45:02 +0545

    Handling Forms

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Example:
+// Add an event handler in the onchange attribute, and let the event handler update the state object:
 
 class MyForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { username: ''};
+    }
+    myChangeHandler = (event) => {
+        this.setState({username: event.target.value});
+    }
     render(){
         return(
             <form>
-                <h1>Hello</h1>
+                <h1>Hello {this.state.username}</h1>
                 <p>Enter your name:</p>
                 <input
                 type="text"
+                onChange={this.myChangeHandler}
                 />
             </form>
         );

56bcccd58aa6ab98c12365281d1ea8bb100bf049
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-11 16:00:03 +0545

    Multiple Input Fields

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Example:
-// Write a form with two input fields:
+//Write a form with two input fields:
 
 class MyForm extends React.Component{
     constructor(props){
         super(props);
-        this.state = { username: '',
+        this.state = { 
+        username: '',
         age: null,
         };
     }
-    mysubmitHandler = (event) => {
-        event.preventDefault();
-        alert("You are submitting " + this.state.username); 
-    }
     myChangeHandler = (event) => {
-        this.setState({username: event.target.value});
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
     }
     render() {
         return(
-            <form onSubmit={this.mysubmitHandler}>
-                <h1>Hello {this.state.username}</h1>
+            <form>
+                <h1>Hello {this.state.username} {this.state.age}</h1>
                 <p>Enter your name, and submit:</p>
                 <input
-                type="text"
-                onChange={this.myChangeHandler}
+                type ="text"
+                name = "username"
+                onChange = {this.myChangeHandler}
                 />
-                <input
-                type= "Submit"
+                <p>Enter the age:</p>
+                <input 
+                type = "text"
+                name = "age"
+                onChange = {this.myChangeHandler}
                 />
             </form>
         );
@@ -35,7 +38,7 @@ class MyForm extends React.Component{
 };
 
 ReactDOM.render(<MyForm />, document.getElementById('root'));
-// Note: We use the same event handler function for both input fields, we could write one event handler for each, but
-// this gives us much cleaner code and is the preferred way in React.
+// Note: We use the same event handler function for both input fields, we could write one event
+// handler for each, but this gives us much cleaner code and is the preferred way in React.
 
 

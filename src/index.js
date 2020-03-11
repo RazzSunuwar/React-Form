b854f70ae823e4f6ebeba02bf63d1eff1377db2f
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-11 22:08:49 +0545

    Select

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Example: A simple textarea with some content initialized in the constructor:
+// Example: A simple select box, where the selected value "Volvo" is initialized in the constructor:
 
 class MyForm extends React.Component{
     constructor(props){
